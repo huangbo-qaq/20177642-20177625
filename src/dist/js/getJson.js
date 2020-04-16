257ada0b6a49ac8890a81c03bb0e1b49b3e99979
黄波 <62139942+huangbo-qaq@users.noreply.github.com>
2020-04-16 18:44:30 +0800

    Add files via upload

--- src/dist/js/getJson.js
+++ src/dist/js/getJson.js
@@ -11,48 +11,63 @@ var treeData = [{
     }]
 }];
 
+function add_div(treeId) {
+    var e = document.getElementById("tree");
+    var div = document.createElement("div");
+    div.id = treeId;
+    div.innerHTML = e.innerHTML;
+    document.getElementById("tree").appendChild(div);
+}
+
 function getData() {
     var texts = document.getElementById("val").value;
-    // 按行分割
-    var row = texts.split('\n');
-    // "导师："，"级博士生："，"级硕士生："，"级本科生："和"、"
-    // 导师
-    var teacher = row[0].split("导师：");
-    // 学生
-    var len = row.length;
-    //var degreeAndGrade = [];
-    var nodes = [];
-    for (var i = 1; i < len; i++)
+    // 按空白行分割不同的树
+    var tree = texts.split('\n\n');
+
+    for (var i = 0; i < tree.length; i++)
     {
-        // 按前后分割
-        var information = row[i].split("：");
-        // 前
-        //degreeAndGrade.push(information[0]);
-        // 后
-        var stuName = information[1].split("、");
-        var nameList = [];
-        for (var j = 0; j < stuName.length; j++)
+        // 第i棵树
+        var row = tree[i].split('\n');
+        // "导师："，"级博士生："，"级硕士生："，"级本科生："和"、"
+        // 导师
+        var teacher = row[0].split("导师：");
+        // 学生
+        var len = row.length;
+        var nodes = [];
+        for (var j = 1; j < len; j++)
         {
-            var name = stuName[j];
-            nameList.push({ "text" : name });
-        }
-        nodes.push(
+            // 按前后分割
+            var information = row[j].split("：");
+            // 前
+            //degreeAndGrade.push(information[0]);
+            // 后
+            var stuName = information[1].split("、");
+            var nameList = [];
+            for (var k = 0; k < stuName.length; k++)
             {
-                "text" : information[0],
-                "nodes" : nameList
+                var name = stuName[k];
+                nameList.push({ "text" : name });
             }
-        );
+            nodes.push(
+                {
+                    "text" : information[0],
+                    "nodes" : nameList
+                }
+            );
+        }
+        treeData[0]["text"] = teacher[1];
+        treeData[0]["nodes"] = nodes;
+
+        var treeId = "tree_" + i;
+        add_div(treeId);
+
+        $("#" + treeId).treeview({
+            color: "#428bca",
+            expandIcon: 'glyphicon glyphicon-chevron-right',
+            collapseIcon: 'glyphicon glyphicon-chevron-down',
+            emptyIcon: 'glyphicon glyphicon-user',
+            showBorder: false,
+            data: treeData
+        });
     }
-
-    treeData[0]["text"] = teacher[1];
-    treeData[0]["nodes"] = nodes;
-
-    $('#tree').treeview({
-        color: "#428bca",
-        expandIcon: 'glyphicon glyphicon-chevron-right',
-        collapseIcon: 'glyphicon glyphicon-chevron-down',
-        emptyIcon: 'glyphicon glyphicon-user',
-        showBorder: false,
-        data: treeData
-    });
 }
